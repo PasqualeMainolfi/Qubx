909e9243fc887ad4629b992c3cfbd20175dcde1c
PasqualeMainolfi <mnlpql@gmail.com>
2024-03-16 17:46:19 +0100

    remove test

--- qubx/src/lib.rs
+++ qubx/src/lib.rs
@@ -7,16 +7,3 @@ mod qubx_pmanage;
 pub use qubx::Qubx;
 pub use qubx_common::StreamParameters;
 pub use qubx_components::*;
-
-
-#[cfg(test)]
-mod test {
-
-    use super::Qubx;
-
-    #[test]
-    fn get_devices() {
-        Qubx::get_devices_info();
-    }
-    
-}
